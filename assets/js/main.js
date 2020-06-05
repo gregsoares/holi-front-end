dc2f4417477e48c37b3a59deb164d65af763c50b
Greg <gregselvagem@gmail.com>
2020-06-04 21:45:09 -0400

    +clickHandler file Todo update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+const { handleClick } = require('./handleClick');
+
 if (document.readyState == 'loading') {
   document.addEventListener('DOMContentLoaded', ready)
 } else {
@@ -15,58 +17,23 @@ function ready() {
       console.debug(e.target)
     })
   }
-
   // Top Nav EL
+  // const top_nav = document.getElementById('link_list');
   // for (let i = 0; i < top_nav.length; i++) {
   //   const element = top_nav[i];
-  //   element.addEventListener('click', e => {
-  //     // handle topNav link click
+  //   element.addEventListener('click', (e) => {
+  //     if (element === brand_name) console.log('EYADL')
   //     console.debug(e.target)
   //   })
   // }
-
-  // Mobile Nav EL
-  // const mobile_nav = document.getElementById('mobile_nav');
-  // top_nav.addEventListener('click', (e) => {
-  //   console.debug(e.target)
-  // })
-  // mobile_nav.parentElement.addEventListener('hover', (e) => {
-  //   console.debug(e.target)
-  // })
-  // console.log(top_nav.)
-}
-
-// Top Nav EL
-// const top_nav = document.getElementById('link_list');
-// for (let i = 0; i < top_nav.length; i++) {
-//   const element = top_nav[i];
-//   element.addEventListener('click', (e) => {
-//     if (element === brand_name) console.log('EYADL')
-//     console.debug(e.target)
-//   })
-// }
-// const brand_name = document.getElementById('brand_name');
-//   for (let i = 0; i < brand_name.length; i++) {
-//     const element = brand_name[i];
-//     element.addEventListener('click', e => {
-//       // handle topNav link click
-//       console.debug(e.target)
-//     })
-//   }
-
-function handleClick() {
-  const body = document.getElementById('section_one');
-  body.innerHTML = `
-  <section id="indexSection" class="flex p-0 m-0 bg-gray-300">
-    <div class=" overflow-visible space-y-3 bg-indigo-200 py-12 my-2 mx-0">
-    Index Section Clicked
-    </div>
-    <div class="flex w-full px-1 mx-2 border-2 border-dotted items-right ">  
-    <div class="text-center border-indigo-300 mx-0 space-y-3 bg-indigo-200 py-12 my-2">
-    Index Section Clicked
-    </div></div> 
-  </section>
-  `
+  // const brand_name = document.getElementById('brand_name');
+  //   for (let i = 0; i < brand_name.length; i++) {
+  //     const element = brand_name[i];
+  //     element.addEventListener('click', e => {
+  //       // handle topNav link click
+  //       console.debug(e.target)
+  //     })
+  //   }
 }
 
 module.exports = handleClick(), ready();
